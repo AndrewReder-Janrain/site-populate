0ab7088bcbb4ad332d564ed887de0c26eb684a98
Andrew Reder <areder@janrain.com>
2015-10-13 11:01:01 -0700

    Script now returns an array of the keys in JSON

--- site-populate.py
+++ site-populate.py
@@ -66,22 +66,24 @@ def build_s3_url(date_string):
 	return url_string
 
 def get_s3_analytics(s3_url):
-
+	s3_key_array = [];
 	s3 = boto.s3.connect_to_region('us-east-1')
-	# s3_bucket = s3.Bucket(s3_url)
 	s3_bucket = s3.get_bucket('janrain.analytics')
-	# print s3_bucket
-	print s3_url
-	# print s3_bucket.get_all_keys(prefix=s3_url)
+
 	for item in s3_bucket.list(prefix=s3_url):
-		print item
-	return
+		temp_string = item.get_contents_as_string()
+		temp_string = temp_string[temp_string.find('{'):-1]
+		s3_key_array.append(yaml.load(temp_string))
+	
+	return s3_key_array
 
 def main():
+	
 	user_list = yaml.load(get_users_with_null_sites())
 	calculate_last_user_events(user_list)
 	s3_url = build_s3_url(user_list[1]['trueDate'])
 	s3_results = get_s3_analytics(s3_url)
+	print s3_results[0]['application_id']
 	
 	return
 
