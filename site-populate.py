762243f062b50678de9717f1cd4ed6b76271473a
Andrew Reder <areder@janrain.com>
2015-10-13 09:29:09 -0700

    Script now gets all users with null sites, calculates the last user event, and gets keys matching that date from s3

---
+++ site-populate.py
@@ -0,0 +1,118 @@
+#!/usr/local/bin/python
+
+import subprocess
+import json
+import yaml
+import dateutil.parser
+import datetime
+import boto
+
+def get_users_with_null_sites():
+
+	apid_array = [
+		"apid-cli",
+		"ef",
+		"user",
+		"-c",
+		"cnhi",
+		"--filter",
+		"sites.siteName is null",
+		"--attributes",
+		'["created","lastUpdated","uuid","sites","lastLogin"]',
+		"--max-results",
+		"10"	
+	]
+
+	jq_array = [
+		"jq",
+		".results"
+	]
+
+	apid_call = subprocess.Popen(apid_array, stdout=subprocess.PIPE)
+	jq_call = subprocess.Popen(jq_array, stdin=apid_call.stdout, stdout=subprocess.PIPE)
+	apid_call.stdout.close()
+	filtered_apid_results = jq_call.communicate()[0]
+	apid_call.wait()
+	#print filtered_apid_results
+	return filtered_apid_results
+
+def calculate_last_user_events(user_list):
+	#print len(user_list)
+	for user in user_list:
+		if user['lastUpdated'] > user['created']:
+			user['trueDate'] = user['lastUpdated']
+		else:
+			user['trueDate'] = user['created']
+	return user_list
+
+def build_s3_url(date_string):
+	temp_date = dateutil.parser.parse(date_string)
+
+	date_string_array = [
+		str(temp_date.year),
+		str(temp_date.month),
+		str(temp_date.day),
+		str(temp_date.hour),
+		'00',
+		'00'
+	]
+
+	url_string = (
+		'capture/entity_create/' 
+		+ reduce(lambda x,y: x + '/' + y, date_string_array) 
+		+ '/fdyc2rm7kvqcnftgyjzsrbawer/'
+	)
+
+	return url_string
+
+def get_s3_analytics(s3_url):
+
+	s3 = boto.s3.connect_to_region('us-east-1')
+	# s3_bucket = s3.Bucket(s3_url)
+	s3_bucket = s3.get_bucket('janrain.analytics')
+	# print s3_bucket
+	print s3_url
+	# print s3_bucket.get_all_keys(prefix=s3_url)
+	for item in s3_bucket.list(prefix=s3_url):
+		print item
+	return
+
+def main():
+	user_list = yaml.load(get_users_with_null_sites())
+	calculate_last_user_events(user_list)
+	s3_url = build_s3_url(user_list[1]['trueDate'])
+	s3_results = get_s3_analytics(s3_url)
+	
+	return
+
+if __name__ == "__main__":
+    main()
+
+# function dateStringToArray {
+# 	tempDateArray=()
+# 	tempDate=$1
+# 	# echo $tempDate
+# 	# $tempDate=echo "${tempDate::-1}"
+# 	echo "${tempDate::-1}"
+# 	# echo $tempDate
+# }
+
+# function getNullSitesUsers {
+# 	echo $(apid-cli ef user -c cnhi --filter "sites.siteName is null" --attributes '["created","lastUpdated","uuid","sites","lastLogin"]' --max-results 10 | jq .results)
+# }
+
+# function getLastUserUpdateTime {
+# 	echo $userList | jq 'map(if .lastUpdated > .created
+# 			then . + {"trueDate":.lastUpdated}
+# 			else . + {"trueDate":.created}
+# 			end
+# 	   )'
+# }
+
+# userList=$(getNullSitesUsers)
+
+# userList=$(getLastUserUpdateTime)
+
+# dateStringToArray "2014-10-09 18:16:31.969098 +0000"
+
+# # echo $userList | jq .
