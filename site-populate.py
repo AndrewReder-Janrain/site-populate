f43c2aa56db49021aa6cf027b8f53c96bc43215d
Andrew Reder <areder@janrain.com>
2015-10-15 11:15:41 -0700

    Script now determines which event types to search, and only looks ahead or behind if timestamp is within 15 minutes of the hour

--- site-populate.py
+++ site-populate.py
@@ -7,6 +7,8 @@ import dateutil.parser
 import datetime
 import boto
 
+json_file = open('test_data.json','w')
+
 def get_users_with_null_sites():
 
 	apid_array = [
@@ -39,55 +41,61 @@ def get_users_with_null_sites():
 def calculate_last_user_events(user_list):
 	#print len(user_list)
 	for user in user_list:
-		if user['lastUpdated'] > user['created']:
-			user['trueDate'] = user['lastUpdated']
-			user['lastEvent'] = 'entity_update'
+		user['events'] = []
+		user['event_times'] = []
+		if user['lastUpdated'] == user['created']:
+			user['events'].append('entity_create')
+			user['event_times'].append(user['created'])
+			# user['trueDate'] = user['lastUpdated']
+			# user['lastEvent'] = 'entity_update'
 		else:
-			user['trueDate'] = user['created']
-			user['lastEvent'] = 'entity_create'
+			user['events'].append('entity_create')
+			user['event_times'].append(user['created'])
+			user['events'].append('entity_update')
+			user['event_times'].append(user['lastUpdated'])
 	return user_list
 
 def build_s3_url(user_object):
-	
-	temp_date = dateutil.parser.parse(user_object['trueDate'])
-	later_date = temp_date + datetime.timedelta(hours=1)
-	earlier_date = temp_date + datetime.timedelta(hours=-1)
-	date_array = [
-		temp_date,
-		later_date,
-		earlier_date
-	]
 	url_string_array = []
-	for date in date_array:	
-		date_string_array = [
-			str(date.year),
-			str(date.month),
-			str(date.day),
-			str(date.hour),
-			'00',
-			'00'
-		]
-		# url_string = (
-		# 	'capture/' + user_object['lastEvent'] + '/'
-		# 	+ reduce(lambda x,y: x + '/' + y, date_string_array) 
-		# 	+ '/fdyc2rm7kvqcnftgyjzsrbawer/'
-		# )
+	for j in range(len(user_object['event_times'])):
+		temp_date = dateutil.parser.parse(user_object['event_times'][j])
+		date_array = [temp_date]
+		if temp_date.minute < 15:
+			date_array.append(temp_date + datetime.timedelta(hours=-1))
+		if temp_date.minute > 45:
+			date_array.append(temp_date + datetime.timedelta(hours=1))
+		
+		for date in date_array:	
+			date_string_array = [
+				str(date.year),
+				str(date.month),
+				str(date.day),
+				str(date.hour),
+				'00',
+				'00'
+			]
+			for i in range(1,len(date_string_array)):
+				if len(date_string_array[i])==1:
+					date_string_array[i]='0'+date_string_array[i]
+			# url_string = (
+			# 	'capture/' + user_object['lastEvent'] + '/'
+			# 	+ reduce(lambda x,y: x + '/' + y, date_string_array) 
+			# 	+ '/fdyc2rm7kvqcnftgyjzsrbawer/'
+			# )
 
-		url_string = (
-			'capture/' + 'entity_create' + '/'
-			+ reduce(lambda x,y: x + '/' + y, date_string_array) 
-			+ '/fdyc2rm7kvqcnftgyjzsrbawer/'
-		)
-		url_string_array.append(url_string)
-		url_string = (
-			'capture/' + 'entity_update' + '/'
-			+ reduce(lambda x,y: x + '/' + y, date_string_array) 
-			+ '/fdyc2rm7kvqcnftgyjzsrbawer/'
-		)
-		url_string_array.append(url_string)
+			url_string = (
+				'capture/' + user_object['events'][j] + '/'
+				+ reduce(lambda x,y: x + '/' + y, date_string_array) 
+				+ '/fdyc2rm7kvqcnftgyjzsrbawer/'
+			)
+			url_string_array.append(url_string)
 	return url_string_array
 
+def log_user_info(info):
+	json_file.write(info)
+
 def get_s3_analytics(s3_url):
+	global json_file
 	#print "URL: " + s3_url
 	s3_key_array = [];
 	s3 = boto.s3.connect_to_region('us-east-1')
@@ -95,6 +103,7 @@ def get_s3_analytics(s3_url):
 	# print s3_bucket.get_all_keys(prefix=s3_url)
 	for item in s3_bucket.list(prefix=s3_url):
 		object_array = []
+		json_file.write('\t\t'+json.dumps(item.name)+'\n')
 		temp_string = item.get_contents_as_string()
 		num_objects = temp_string.count('\n')
 		for j in range(num_objects):
@@ -104,38 +113,25 @@ def get_s3_analytics(s3_url):
 			if temp_string.find('\n') > -1:
 				s3_key_array.append(yaml.load(temp_string[temp_string.find('{'):temp_string.find('\n')]))
 				temp_string = temp_string[temp_string.find('\n')+1:-1]
-			# else:
-			# 	# print temp_string
-			# 	try:
-			# 		s3_key_array.append(yaml.load(temp_string[temp_string.find('{'):-1]))
-			# 	except:
-			# 		print "Wah Wah: " + item.name
-			# 		s3_key_array.append(yaml.load(temp_string[temp_string.find('{'):temp_string.rfind("}")]))
-			
-			
 
-
-		# try:
-		# 	s3_key_array.append(yaml.load(temp_string))
-		# except:
-		# 	# temp_string = temp_string[temp_string.find('{'):-1]
-		# 	print s3_url
-		# 	print item
-		# 	# s3_key_array.append(yaml.load(temp_string))
-	# print s3_key_array
 	return s3_key_array
 
 def find_s3_event(s3_result_set, user):
 	return
 
 def main():
-	json_file = open('test_data.json','w')
+	global json_file
+	# json_file = open('test_data.json','w')
 	user_list = yaml.load(get_users_with_null_sites())
 	calculate_last_user_events(user_list)
 	s3_results = []
 	for i in range(2):	
+		json_file.write(json.dumps(user_list[i])+'\n')
+		
+
 		s3_url_array = build_s3_url(user_list[i])
 		for s3_url in s3_url_array:
+			json_file.write('\t'+json.dumps(s3_url)+'\n')
 			s3_results += get_s3_analytics(s3_url)
 		log_string = str(i) + ": "
 		try:
@@ -143,8 +139,8 @@ def main():
 		except:
 			log_string += "No results for: " + user_list[i]['trueDate']
 		print log_string
-	json_string = '[' + ','.join(json.dumps(result) for result in s3_results) + ']'
-	json_file.write(json_string)
+	# json_string = '[' + ','.join(json.dumps(result) for result in s3_results) + ']'
+	# json_file.write(json_string)
 	json_file.close()
 	# print len(s3_results)
 	return
@@ -152,31 +148,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-# function dateStringToArray {
-# 	tempDateArray=()
-# 	tempDate=$1
-# 	# echo $tempDate
-# 	# $tempDate=echo "${tempDate::-1}"
-# 	echo "${tempDate::-1}"
-# 	# echo $tempDate
-# }
-
-# function getNullSitesUsers {
-# 	echo $(apid-cli ef user -c cnhi --filter "sites.siteName is null" --attributes '["created","lastUpdated","uuid","sites","lastLogin"]' --max-results 10 | jq .results)
-# }
-
-# function getLastUserUpdateTime {
-# 	echo $userList | jq 'map(if .lastUpdated > .created
-# 			then . + {"trueDate":.lastUpdated}
-# 			else . + {"trueDate":.created}
-# 			end
-# 	   )'
-# }
-
-# userList=$(getNullSitesUsers)
-
-# userList=$(getLastUserUpdateTime)
-
-# dateStringToArray "2014-10-09 18:16:31.969098 +0000"
-
-# # echo $userList | jq .
